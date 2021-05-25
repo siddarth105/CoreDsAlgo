020cef273a1511dbcf807cb6e6c2401384b0a999
siddarth105 <zealio.z@gmail.com>
2021-05-25 17:14:46 +0530

    BFS & DFS

---
+++ src/Algo/BFS/WaterSupplyPbm.java
@@ -0,0 +1,5 @@
+package Algo.BFS;
+
+// https://www.geeksforgeeks.org/implementing-water-supply-problem-using-breadth-first-search/?ref=rp
+public class WaterSupplyPbm {
+}
---
+++ src/Algo/DFS/WaterConnectionPbm.java
@@ -0,0 +1,5 @@
+package Algo.DFS;
+
+// https://www.geeksforgeeks.org/water-connection-problem/?ref=rp
+public class WaterConnectionPbm {
+}
